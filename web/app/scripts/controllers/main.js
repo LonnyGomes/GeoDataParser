0b8a5ef5474b3e01a6fd2c35d88ccbc90b5df626
Lonny Gomes <code@lonnygomes.com>
2015-03-18 23:02:03 -0400

    Cleaned up the files and removed uneeded menus

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -11,11 +11,8 @@ angular.module('geonamesFilterApp')
     'use strict';
 
     $scope.selected = undefined;
-    $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-  // Any function returning a promise object can be used to load values asynchronously
 
     geoNamesFactory.geoData.then(function (data) {
-      console.dir(data[0]);
       $scope.geoData = data;
     });
   });
