118ace98c8c642c0931eb6418aa99838875de4a0
Lonny Gomes <code@lonnygomes.com>
2015-03-19 00:01:12 -0400

    Incorporated leaflet map that plots found locations

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -1,4 +1,4 @@
-/*global angular */
+/*global angular, L */
 /**
  * @ngdoc function
  * @name geonamesFilterApp.controller:MainCtrl
@@ -14,5 +14,29 @@ angular.module('geonamesFilterApp')
 
     geoNamesFactory.geoData.then(function (data) {
       $scope.geoData = data;
+    });
+
+    //TODO: make map compentent it's own directive
+    var map = L.map('map', {
+      center: [20.0, 5.0],
+      minZoom: 2,
+      zoom: 2
+    });
+
+    L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+      subdomains: ['otile1', 'otile2', 'otile3', 'otile4']
+    }).addTo(map);
+
+    $scope.$watch("selected", function (newVal, oldVal) {
+      //if a new country value was selected,
+      //plot marker and zoom to the location
+      if (newVal && typeof newVal === "object") {
+        var coords = [newVal.lat, newVal.lon];
+        L.marker(coords)
+          .bindPopup(newVal.name)
+          .addTo(map);
+
+        map.setView(coords, 6, { animate: true });
+      }
     });
   });
